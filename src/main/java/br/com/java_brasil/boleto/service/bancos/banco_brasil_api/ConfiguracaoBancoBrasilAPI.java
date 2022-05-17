49c04b8cb17e9d4f1fafbde8e64ad18f30960abd
Vitorhr10 <vitorrodrigueshr10@gmail.com>
2022-05-16 23:17:49 -0300

    feat: Definição dos campos obrigatórios especificado pelo Banco do Brasil

--- src/main/java/br/com/java_brasil/boleto/service/bancos/banco_brasil_api/ConfiguracaoBancoBrasilAPI.java
+++ src/main/java/br/com/java_brasil/boleto/service/bancos/banco_brasil_api/ConfiguracaoBancoBrasilAPI.java
@@ -1,41 +1,61 @@
 package br.com.java_brasil.boleto.service.bancos.banco_brasil_api;
 
-import br.com.java_brasil.boleto.exception.BoletoException;
 import br.com.java_brasil.boleto.model.Configuracao;
 import br.com.java_brasil.boleto.model.enums.AmbienteEnum;
 import lombok.Getter;
 import lombok.Setter;
-import org.apache.commons.lang3.StringUtils;
+
+import javax.validation.constraints.NotEmpty;
+import java.util.Arrays;
+import java.util.List;
 
 @Getter
 @Setter
 public class ConfiguracaoBancoBrasilAPI implements Configuracao {
 
+    @NotEmpty
     private String clientId;
+    @NotEmpty
     private String clientSecret;
+    @NotEmpty
     private String developerKey;
+    @NotEmpty
     private String authorization;
+    @NotEmpty
     private AmbienteEnum ambiente;
+    @NotEmpty
     private String urlRegistroBoleto = "/boletos?gw-dev-app-key=";
+    @NotEmpty
     private String urlTokenProducao = "https://oauth.bb.com.br/oauth/token";
+    @NotEmpty
     private String urlTokenHomologacao = "https://oauth.hm.bb.com.br/oauth/token";
+    @NotEmpty
     private String urlBaseProducao = "https://api.bb.com.br/cobrancas/v2";
+    @NotEmpty
     private String urlBaseHomologacao = "https://api.hm.bb.com.br/cobrancas/v2";
 
     @Override
-    public void verificaConfiguracoes() {
-        if (StringUtils.isBlank(clientId) ||
-                StringUtils.isBlank(clientSecret) ||
-                StringUtils.isBlank(developerKey) ||
-                ambiente == null ||
-                StringUtils.isBlank(urlRegistroBoleto) ||
-                StringUtils.isBlank(urlTokenProducao) ||
-                StringUtils.isBlank(urlTokenHomologacao) ||
-                StringUtils.isBlank(urlBaseProducao) ||
-                StringUtils.isBlank(urlBaseHomologacao) ||
-                StringUtils.isBlank(authorization)) {
-            throw new BoletoException("Configuracoes invalidas.");
-        }
+    public List<String> camposObrigatoriosBoleto() {
+        return Arrays.asList(
+                "numeroConvenio",
+                "numeroCarteira",
+                "numeroVariacaoCarteira",
+                "codigoModalidade",
+                "dataVencimento",
+                "valorOriginal",
+                "codigoAceite",
+                "codigoTipoTitulo",
+                "indicadorPermissaoRecebimentoParcial",
+                "numeroTituloCliente",
+                "pagador.tipoInscricao",
+                "pagador.numeroInscricao",
+                "pagador.nome",
+                "pagador.endereco.logradouro",
+                "pagador.endereco.bairro",
+                "pagador.endereco.cidade",
+                "pagador.endereco.cep",
+                "pagador.endereco.uf",
+                "pagador");
     }
 
     public String getURLBase() {
